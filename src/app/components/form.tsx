5c34a484b0fa6fa4ffa2f1f67a5e795ba298cc90
The-XENO-Studios <thexenostudios@gmail.com>
2024-04-14 13:40:24 +0600

    Tweak

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -1,11 +1,6 @@
 "use client";
 
 import { cn } from "@/utils/cn";
-import {
-  IconBrandGithub,
-  IconBrandGoogle,
-  IconBrandOnlyfans,
-} from "@tabler/icons-react";
 import { Label } from "@radix-ui/react-label";
 import { Input } from "./ui/input";
 
@@ -80,7 +75,7 @@ export function Form() {
           className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
-          Publish &rarr;
+          Publish
           <BottomGradient />
         </button>
       </form>
