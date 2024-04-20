2ece26ccda30ed8124d03390ade8c632e6f25290
The-XENO-Studios <thexenostudios@gmail.com>
2024-04-20 17:16:42 +0600

    Explore Button Transition

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -29,11 +29,11 @@ const Hero = () => {
           <div className="my-6 flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
             <Link
               href="/market"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
+              className="group inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900"
             >
               Explore now
               <svg
-                className="w-3.5 h-3.5 ms-2 rtl:rotate-180"
+                className="w-3.5 h-3.5 ms-2 rtl:rotate-180 transition-all group-hover:translate-x-5 group-hover:opacity-0"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
