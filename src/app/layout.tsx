05c95f96450b939e065687d215ccf4169d57009a
The-XENO-Studios <thexenostudios@gmail.com>
2024-09-14 21:13:28 +0600

    Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "NDCFT",
+  title: "XENFT",
   description: "Buy NFTs of your choice",
   icons: { icon: "/Logo.png" },
 };
